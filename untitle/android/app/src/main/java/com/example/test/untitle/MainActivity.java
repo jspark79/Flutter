6dd52cf890ff43338e233c56dc57d6b17c93dbb1
Jongseo Park <pjsysh131214@gmail.com>
2022-10-25 11:29:46 +0900

    - Flutter Test Code

---
+++ untitle/android/app/src/main/java/com/example/test/untitle/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.test.untitle;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ untitle/lib/main.dart
@@ -0,0 +1,96 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Flutter layout demo'),
+          ),
+                          body: Column(
+                            children: [
+                              titleSection,
+                            ],
+    ),
+    ),
+    );
+  }
+}
+
+Widget titleSection = Container(
+  padding: const EdgeInsets.all(32),
+  child: Row(
+    children: [
+      Expanded(
+        /*1*/
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            /*2*/
+            Text(
+              'sdfsfd, sfsdff',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            // Container(
+            //   padding: const EdgeInsets.only(bottom: 8),
+            //   child: Text(
+            //     'Oeschinen Lake Campground',
+            //     style: TextStyle(
+            //       fontWeight: FontWeight.bold,
+            //     ),
+            //   ),
+            // ),
+            Text(
+              'Kandersteg, Switzerland',
+              style: TextStyle(
+                color: Colors.grey[500],
+              ),
+            ),
+          ],
+        ),
+      ),
+      /*3*/
+      Icon(
+        Icons.star,
+        color: Colors.red[200]
+      ),
+      Text('41'),
+    ],
+  ),
+);
+
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        BlueBox(),
+        BlueBox(),
+        BlueBox(),
+      ],
+    );
+  }
+}
+class BlueBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        border: Border.all(),
+      ),
+    );
+  }
+}
